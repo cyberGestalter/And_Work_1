88e7dabf4618c28ea14cff1423a98b984e5782fa
cyberGestalter <minto31415@mail.ru>
2021-07-16 20:05:28 +0300

    reaction for cat image is added

--- app/src/main/java/com/example/and_work_1/MainActivity.kt
+++ app/src/main/java/com/example/and_work_1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.and_work_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     fun closeApp(view: View) {
         finish()
+    }
+
+    fun tapCat(view: View) {
+        Toast.makeText(this, getString(R.string.activity_main_cat_toast), Toast.LENGTH_SHORT)
+            .show()
     }
 }
